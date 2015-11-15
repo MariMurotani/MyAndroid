0ab9e9709b1f367e831216905ddfc6844ef52d03
Mari Murotani <muromari5123@gmail.com>
2015-11-15 12:31:13 +0900

    Added class for testing dynamic loader

---
+++ app/src/main/java/com/example/mari/myandroid/TestDinamicLoad.java
@@ -0,0 +1,15 @@
+package com.example.mari.myandroid;
+
+/**
+ * Created by murotanimari on 2015/11/15.
+ */
+public class TestDinamicLoad {
+    public String strURL = "http://test.com/aaa";
+
+    public TestDinamicLoad() {
+    }
+
+    public String testResult(){
+        return strURL;
+    }
+}
